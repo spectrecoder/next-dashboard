3bb97e879ba9640300dc493ad35a25869f7a5b31
spectrecoder <pong96111kind@gmail.com>
2024-05-09 09:25:38 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import { inter, lusitana } from './ui/fonts';
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -23,9 +28,24 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <div className={styles.shape}></div>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt='Screenshots'
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className='hidden mc:block'
+            alt='mobile screenshots'
+          />
         </div>
       </div>
     </main>
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,9 +1,17 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import { stat } from 'fs';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
+      // className={clsx(
+      //   'inline-flex items-center rounded-full px-2 py-1 text-xs',
+      //   {
+      //     'bg-gray-100 text-gray-500': status === 'pending',
+      //     'bg-green-500 text-white': status === 'paid',
+      //   },
+      // )}
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
@@ -20,7 +28,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
       ) : null}
       {status === 'paid' ? (
         <>
-          Paid
+          Paid  
           <CheckIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
